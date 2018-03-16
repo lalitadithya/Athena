edd950c0194ddf3210306b21c2a0472e6857615f
Lalit Adithya <lalitadithya5071@gmail.com>
2018-03-16 12:42:51 +0530

    Added cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Execution } from '../models/execution';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  executions: Execution[] = [
+    { id: "1", name: "Regression", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "2", name: "Neural Network", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "3", name: "Clustering", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "4", name: "Classification", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "5", name: "Recommendation", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "6", name: "Reinforcement", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "7", name: "Anomaly detection", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "8", name: "Density estimation", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "9", name: "Regression", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg"},
+    { id: "10", name: "Neural Network", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare turpis mattis tellus sodales posuere. Mauris ac nunc dictum, tincidunt erat in, commodo nisi.",status: "Running", image:"https://material.angular.io/assets/img/examples/shiba2.jpg" }
+  ];
 
   constructor() { }
 
---
+++ src/app/models/execution.ts
@@ -0,0 +1,7 @@
+export class Execution {
+    id: String;
+    name: String;
+    description: String;
+    status: String;
+    image: String;
+}
