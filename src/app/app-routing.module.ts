79f8818e451177c64a49e7da6b6348b3069b8bb9
Lalit Adithya <lalitadithya5071@gmail.com>
2018-03-25 17:16:37 +0530

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DataScientistComponent } from './data-scientist/data-scientist.component';
 import { AuthenticationGuard } from './authentication.guard';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'data-scientist', component: DataScientistComponent},
 ];
 
